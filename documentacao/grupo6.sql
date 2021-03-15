69a75b5ed82cdb78fca28633f515881277646fc4
igorpdev <igorjps@uol.com.br>
2021-03-15 13:33:15 -0300

    documentação corrigida

--- documentacao/grupo6.sql
+++ documentacao/grupo6.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `postagem` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`titulo` varchar(255),
-	`data` DATETIME AUTO_INCREMENT,
-	`conteudo` varchar(255),
+	`titulo` varchar(255) NOT NULL,
+	`data` DATETIME NOT NULL AUTO_INCREMENT,
+	`conteudo` varchar(255) NOT NULL,
 	`midia` VARCHAR(255) NOT NULL,
 	`tema_id` INT NOT NULL,
 	`usuario_id` INT NOT NULL,
@@ -11,17 +11,17 @@ CREATE TABLE `postagem` (
 
 CREATE TABLE `tema` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`cursos` varchar(255),
-	`vagas` varchar(255),
-	`networking` varchar(255),
+	`cursos` varchar(255) NOT NULL,
+	`vagas` varchar(255) NOT NULL,
+	`networking` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `usuario` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`nome` varchar(255),
-	`email` varchar(255),
-	`senha` varchar(255),
+	`nome` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`senha` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
