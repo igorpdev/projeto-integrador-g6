693768f4f917c60695baec0776783cc5c4be3d68
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-03-22 10:03:06 -0300

    task 3

---
+++ adaTech/src/main/java/com/egaliteSiX/adaTech/AdaTechApplication.java
@@ -0,0 +1,13 @@
+package com.egaliteSiX.adaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaTechApplication.class, args);
+	}
+
+}
