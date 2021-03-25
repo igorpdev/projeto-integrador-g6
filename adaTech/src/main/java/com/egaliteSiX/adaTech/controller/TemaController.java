0a96b2723a6bbb028dfab73c57b4bb2bc3d25ecd
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-03-25 11:34:08 -0300

    TemaController 1.1

--- adaTech/src/main/java/com/egaliteSiX/adaTech/controller/TemaController.java
+++ adaTech/src/main/java/com/egaliteSiX/adaTech/controller/TemaController.java
@@ -37,7 +37,7 @@ public class TemaController {
         return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/tema/{descricao}")
+    @GetMapping("/descricao/{descricao}")
     public ResponseEntity <List<Tema>> getByDescricao(@PathVariable String descricao) {
         return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
     }
