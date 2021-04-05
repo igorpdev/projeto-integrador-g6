bfa18eca2729dce9f4870cbbd164abeed5ccdc24
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-04-05 09:40:57 -0300

    alteracao nome

--- beequalite/src/main/java/com/egaliteSiX/beequalite/BeequaliteApplication.java
+++ beequalite/src/main/java/com/egaliteSiX/beequalite/BeequaliteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdaTechApplication {
+public class BeequaliteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdaTechApplication.class, args);
+		SpringApplication.run(BeequaliteApplication.class, args);
 	}
 
 }
