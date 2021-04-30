b47faadfe8b4a34c7fda22449ed9ab83b0bc2fc0
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-04-30 20:06:04 -0300

    adicionadas rotas de componentes cadastro e sobre

--- beequalite/src/app/app-routing.module.ts
+++ beequalite/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CadastroComponent, pathMatch: 'full'},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'sobre', component: SobreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
