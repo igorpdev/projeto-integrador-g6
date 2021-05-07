0378f0717581de70abbeaf3a911b99cfe8b2c92e
johnny <jal.johnny13@mail.com>
2021-05-07 15:18:49 -0300

    get-post-tema

--- beequalite/src/app/app-routing.module.ts
+++ beequalite/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { SobreComponent } from './sobre/sobre.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
+  
   {path: '', component: CadastroComponent, pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
   {path: 'sobre', component: SobreComponent},
-  {path: 'inspire-se', component: InspireComponent}
+  {path: 'inspire-se', component: InspireComponent},
+  {path: 'tema', component: TemaComponent}
+  
 ];
 
 @NgModule({
--- beequalite/src/app/tema/tema.component.ts
+++ beequalite/src/app/tema/tema.component.ts
@@ -1,6 +1,10 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
+
 
 @Component({
   selector: 'app-tema',
@@ -9,8 +13,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -18,7 +26,20 @@ export class TemaComponent implements OnInit {
       this.router.navigate(['/login'])   
       // <!-- entrar ou login -->
     }
-  
+  this.findAllTemas()
+  }
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp 
+    })
   }
 
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=> {
+      this.tema=resp
+      alert('Tema cadastrado com sucesso')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
+  }
 }
